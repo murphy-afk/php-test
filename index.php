c9c06be25e27473c862e9361c486d98b1604db0d
murphy-afk <bardarenoemi@gmail.com>
2026-03-03 17:11:37 +0100

    number check

--- index.php
+++ index.php
@@ -4,23 +4,28 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <title>Document</title>
 </head>
 
 <body>
-  <h2>odd or even?</h2>
+  <h2>guess the number</h2>
   <form action="index.php" method="get">
     <label for="number">Enter a number</label>
     <input type="number" name="number">
     <button type="submit">Check</button>
     <?php
     $number = $_GET['number'] ?? null;
-    $result = $number % 2 == 0 ? 'even' : 'odd';
+    $randomNumber = rand(1, 10);
+    $result = $number == $randomNumber ? 'correct' : 'wrong the number was: ' . $randomNumber;
     ?>
   </form>
-  <?php
-    echo $number != null ? 'the number ' . $number . ' is ' . $result : 'enter a number';
-  ?>
+  <hr>
+  <p>
+    <?php
+    echo $number != null ? 'your guess (' . $number . ') is ' . $result : 'enter a number';
+    ?>
+  </p>
 </body>
 
 </html>
